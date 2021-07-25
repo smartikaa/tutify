035ed2472feab0737ab29d0425f1c494cfae8ac1
smartikaa <smarikasingh9a@gmail.com>
2021-07-25 12:05:11 +0530

    Made the navigation bar

--- index.php
+++ index.php
@@ -6,10 +6,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/bootstrap.min.css">
     <link rel="stylesheet" href="./css/all.min.css">
-
-    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/style.css"
+    <title></title>
 </head>
 <body>
+    <!-- start navigation -->
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark pl-5">
+        <a class="navbar-brand" href="./index.php">Tutify</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav custom-nav  pl-5"> 
+                <a class="nav-item nav-link active custom-nav-item" href="index.php">Home </a>
+                <a class="nav-item nav-link custom-nav-item" href="#">Courses</a>
+                <a class="nav-item nav-link custom-nav-item" href="#">My Profile</a>
+                <a class="nav-item nav-link custom-nav-item" href="#">Logout</a>
+                <a class="nav-item nav-link custom-nav-item" href="#">Signup</a>
+                <a class="nav-item nav-link custom-nav-item" href="#">Contact</a>
+            </div>
+        </div>
+    </nav>
+
+    <!-- end navigation -->
     <script src="./js/jquery.min.js"></script>
     <script src="./js/popper.min.js"></script>
     <script src="./js/bootstrap.min.js"></script>
